94f6a8e792f0c4685734889380f7d8f10a892d90
Artur Sultanov <sultanov.personal@gmail.com>
2023-04-17 17:35:51 +0200

    update

--- uart_rx.vhd
+++ uart_rx.vhd
@@ -25,20 +25,19 @@ architecture behavioral of UART_RX is
 
 
 	-- FSM outputs
-    signal read_en_fsm : std_logic;
-    signal clk_cnt_en_fsm : std_logic;
-    signal valid_fsm : std_logic;
+    signal read_en : std_logic;
+    signal clk_cnt_en : std_logic;
+    signal valid : std_logic;
     -- FSM inputs
     signal data_fsm : std_logic;
     signal bit_cnt : std_logic_vector(3 downto 0) := "0000";
     signal clk_cnt : std_logic_vector(4 downto 0) := "00000";
     -- logic outputs
     signal xor_out : std_logic;
+    signal and_out : std_logic;
     signal not_out : std_logic;
     signal cmp_equal : std_logic;
-    signal and_out : std_logic;
-
-
+    
 begin
 
     -- Instance of RX FSM
@@ -52,10 +51,17 @@ begin
         BIT_CNT => bit_cnt,
         CLK_CNT => clk_cnt,
         --OUPUTS
-        READ_EN => read_en_fsm,
-        CLK_CNT_EN => clk_cnt_en_fsm,
-        VALID => valid_fsm
+        READ_EN => read_en,
+        CLK_CNT_EN => clk_cnt_en,
+        VALID => valid
     );
+
+    
+    -- Logic gates
+    xor_out <= clk_cnt_en xor and_out;
+    and_out <= read_en and cmp_equal;
+    not_out <= not xor_out;
+    cmp_equal <= '1' when clk_cnt = "10000" else '0';
 
 
 end architecture;
