396bce088cb00eb809ea5546c464198a9155d056
Artur Sultanov <sultanov.personal@gmail.com>
2023-04-18 08:53:18 +0200

    update

--- uart_rx.vhd
+++ uart_rx.vhd
@@ -18,7 +18,7 @@ entity UART_RX is
     );
 end entity;
 
------------------------------------
+
 
 -- Architecture implementation (INSERT YOUR IMPLEMENTATION HERE)
 architecture behavioral of UART_RX is
@@ -29,7 +29,6 @@ architecture behavioral of UART_RX is
     signal clk_cnt_en : std_logic;
     signal valid : std_logic := '0';
     -- FSM inputs
-    --signal data_fsm : std_logic;
     signal bit_cnt : std_logic_vector(3 downto 0) := "0000";
     signal clk_cnt : std_logic_vector(3 downto 0) := "0000";
     -- logic outputs
@@ -37,12 +36,19 @@ architecture behavioral of UART_RX is
     signal and_out : std_logic := '0';
     signal not_out : std_logic;
     signal cmp_equal : std_logic;
+    -- shift register output
+    signal shift_out : std_logic_vector(7 downto 0);
+
     
 begin
 
+    -- Logic gates
+    and_out <= read_en and cmp_equal;
+    xor_out <= clk_cnt_en xor and_out;
+    not_out <= not xor_out;
+
     -- Instance of RX FSM
     fsm: entity work.UART_RX_FSM
-    --fsm: entity work.UART_RX_FSM(behavioral)
     port map (
         CLK => CLK,
         RST => RST,
@@ -55,30 +61,31 @@ begin
         CLK_CNT_EN => clk_cnt_en,
         VALID => valid
     );
-       -- CLK counter
-       p_clk_cnt : process (CLK)
-       begin
-           if rising_edge(CLK) then
-               if xor_out = '1' then
-                   clk_cnt <= clk_cnt + 1;
-               else
-                   clk_cnt <= "0000";
-               end if;
-           end if;
-       end process;
-    
-       -- CMP equal
-        p_cmp_equal : process (clk_cnt)
-        begin
-            if clk_cnt = "1111" then
-                cmp_equal <= '1';
+       
+    -- CLK counter
+    p_clk_cnt : process (CLK)
+    begin
+        if rising_edge(CLK) then
+            if xor_out = '1' then
+                clk_cnt <= clk_cnt + 1;
             else
-                cmp_equal <= '0';
+                clk_cnt <= "0000";
             end if;
-        end process;
+        end if;
+    end process;
+
+    -- CMP equal
+    p_cmp_equal : process (clk_cnt)
+    begin
+        if clk_cnt = "1111" then
+            cmp_equal <= '1';
+        else
+            cmp_equal <= '0';
+        end if;
+    end process;
 
 
-    	-- Bit counter
+    -- Bit counter
 	p_clk_bits : process (CLK)
 	begin
 		if rising_edge(CLK) then
@@ -95,21 +102,18 @@ begin
 
     -- Shift register
     p_shift_register : process (CLK)
-		variable shift_out : std_logic_vector(7 downto 0);
+		--variable shift_out : std_logic_vector(7 downto 0);
 	begin
 		if rising_edge(CLK) then
 			if and_out = '1' then
-				shift_out := DIN & shift_out(7 downto 1);
-                   DOUT <= shift_out;
-                --shift_out <= shift_out(6 downto 0) & DIN;
+				--shift_out := DIN & shift_out(7 downto 1);
+                shift_out <= shift_out(6 downto 0) & DIN;
+                   --DOUT <= shift_out;
 			end if;
 		end if;
 	end process;
 
-    -- Logic gates
-    and_out <= read_en and cmp_equal;
-    xor_out <= clk_cnt_en xor and_out;
-    not_out <= not xor_out;
 
+    DOUT <= shift_out;
     DOUT_VLD <= valid;
 end architecture;
