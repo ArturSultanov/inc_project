e9e007b1175b61d3e6760e37e0cfb840e9bc424f
Artur Sultanov <sultanov.personal@gmail.com>
2023-04-19 19:37:07 +0200

    update; created ALT version

---
+++ Alt version/uart_rx.vhd
@@ -0,0 +1,111 @@
+-- uart_rx.vhd: UART controller - receiving (RX) side
+-- Author(s): Artur Sultanov (xsulta01)
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+
+
+-- Entity declaration (DO NOT ALTER THIS PART!)
+entity UART_RX is
+    port(
+        CLK      : in std_logic;
+        RST      : in std_logic;
+        DIN      : in std_logic;
+        DOUT     : out std_logic_vector(7 downto 0);
+        DOUT_VLD : out std_logic
+    );
+end entity;
+
+
+
+-- Architecture implementation (INSERT YOUR IMPLEMENTATION HERE)
+architecture behavioral of UART_RX is
+
+
+	-- FSM outputs
+    signal read_en : std_logic;
+    signal clk_cnt_rst : std_logic;
+    signal valid : std_logic := '0';
+    -- FSM inputs
+    signal bit_cnt : std_logic_vector(3 downto 0) := "0000";
+    signal clk_cnt : std_logic_vector(3 downto 0) := "0000";
+    -- logic outputs
+    signal or_out : std_logic;
+    signal and_out : std_logic := '0';
+    signal cmp_equal : std_logic;
+    -- shift register output
+    signal shift_out : std_logic_vector(7 downto 0);
+
+begin
+
+    -- Logic gates
+    and_out <= read_en and cmp_equal;
+    or_out <= clk_cnt_rst or and_out;
+
+    -- Instance of RX FSM
+    fsm: entity work.UART_RX_FSM
+    port map (
+        CLK => CLK,
+        RST => RST,
+        -- INPUTS 
+        DIN => DIN,
+        BIT_CNT => bit_cnt,
+        CLK_CNT => clk_cnt,
+        --OUPUTS
+        READ_EN => read_en,
+        CLK_CNT_RSRT => clk_cnt_rst,
+        VALID => valid
+    );
+       
+    -- CLK counter
+    p_clk_cnt : process (CLK)
+    begin
+        if rising_edge(CLK) then
+            if or_out = '0' then
+                clk_cnt <= clk_cnt + 1;
+            else
+                clk_cnt <= "0000";
+            end if;
+        end if;
+    end process;
+
+    -- CMP equal (finding mid-bit)
+    p_cmp_equal : process (clk_cnt)
+    begin
+        if clk_cnt = "1111" then
+            cmp_equal <= '1';
+        else
+            cmp_equal <= '0';
+        end if;
+    end process;
+
+    -- Bit counter
+	p_clk_bits : process (CLK)
+	begin
+		if rising_edge(CLK) then
+			if valid = '1' then
+				bit_cnt <= "0000";
+			else
+				if and_out = '1' then
+					bit_cnt <= bit_cnt + 1;
+				end if;
+			end if;
+		end if;
+	end process;
+
+    -- Shift register
+    p_shift_register : process (CLK)
+	begin
+		if rising_edge(CLK) then
+			if and_out = '1' then
+                shift_out <= DIN & shift_out(7 downto 1);
+			end if;
+		end if;
+	end process;
+
+    -- Outputs            
+    DOUT <= shift_out;
+    DOUT_VLD <= valid;
+end architecture;
---
+++ Alt version/uart_rx_fsm.vhd
@@ -0,0 +1,98 @@
+-- uart_rx_fsm.vhd: UART controller - finite state machine controlling RX side
+-- Author(s): Artur Sultanov (xsulta01)
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+
+
+entity UART_RX_FSM is
+    port(
+       CLK : in std_logic;
+       RST : in std_logic;
+       -- INPUTS
+       DIN : in std_logic;
+       BIT_CNT : in std_logic_vector(3 downto 0);
+       CLK_CNT : in std_logic_vector(3 downto 0);
+       --OUPUTS
+       READ_EN : out std_logic;
+       CLK_CNT_RSRT : out std_logic;
+       VALID : out std_logic
+    );
+end entity;
+
+
+
+architecture behavioral of UART_RX_FSM is
+    type state_type is (WAIT_FOR_START, WAIT_FOR_MID_BIT, CLK_CNT_RST, READING_DATA, WAIT_FOR_STOP, VALIDATING);
+    signal current_state, next_state : state_type;
+begin
+
+    -- State switching logic
+    p_state_switch : process (CLK, RST)
+    begin
+        if rising_edge(CLK) then
+            if RST = '1' then
+                current_state <= WAIT_FOR_START;
+            else 
+                current_state <= next_state;
+            end if;
+        end if;
+    end process;
+
+    p_next_state_selecor : process (current_state, DIN, BIT_CNT, CLK_CNT)
+    begin
+        case current_state is
+            when WAIT_FOR_START => -- waiting for start-bit.
+                READ_EN <= '0';
+                CLK_CNT_RSRT <= '1';
+                VALID <= '0';
+                if DIN = '0' then -- start-bit is logic '0'.
+                    next_state <= WAIT_FOR_MID_BIT;
+                end if;
+
+            when WAIT_FOR_MID_BIT =>
+                READ_EN <= '0';
+                CLK_CNT_RSRT <= '0'; -- CLK counter in unlimited mode.
+                VALID <= '0';
+                if CLK_CNT = "0111" then -- waiting 7 clk to get a mid-bit of start-bit.
+                    next_state <= CLK_CNT_RST;
+                end if;
+
+            when CLK_CNT_RST => -- restart CLK counter.  
+                READ_EN <= '0';
+                CLK_CNT_RSRT <= '1';
+                VALID <= '0';
+                next_state <= READING_DATA;
+
+            when READING_DATA => -- here is the mid-bit of start-bit. Start reading data each 16 CLK.
+                READ_EN <= '1'; -- when READ_EN = '1', CLK counter is limited by 16 CLK, then is restarted.
+                CLK_CNT_RSRT <= '0';  
+                VALID <= '0';              
+                if BIT_CNT = "1000" then -- have read 8 data-bits.
+                    next_state <= WAIT_FOR_STOP;
+                end if;
+
+            when WAIT_FOR_STOP =>
+                READ_EN <= '0';
+                CLK_CNT_RSRT <= '0';
+                VALID <= '0';
+                if CLK_CNT = "1111" then
+                    if DIN = '1' then
+                        next_state <= VALIDATING;
+                    else
+                        next_state <= WAIT_FOR_START;
+                    end if;
+                end if;
+
+            when VALIDATING =>
+                READ_EN <= '0';
+                CLK_CNT_RSRT <= '1';
+                VALID <= '1';
+                next_state <= WAIT_FOR_START;
+            when others => 
+                next_state <= WAIT_FOR_START;
+        end case; 
+    end process;
+end architecture;
